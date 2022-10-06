b5dc13ec99a85847dd5036f1ba6136661a0ab527
Дарья Шарипова <darya.sharipova@oldcodex.ru>
2022-10-06 20:49:03 +0500

    переотправка

--- Program.cs
+++ Program.cs
@@ -1,8 +1,1 @@
-﻿//17
-Console.WriteLine("Введите первый катет");
-double a = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите второй катет");
-double b = double.Parse(Console.ReadLine());
-double c = Math.Sqrt(a *a + b * b);
-double h = (a * b)/c;
-Console.WriteLine(h);
+﻿
